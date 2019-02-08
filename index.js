5d0979d7ad19dd2abb40214bb353f40c9cf4c582
LayRT <laylet.rterrazas@gmail.com>
2019-02-07 21:39:47 -0600

    hola

---
+++ index.js
@@ -0,0 +1,8 @@
+var cowsay = require("cowsay");
+console.log(cowsay.say({
+	text : "Soy una vaquita pirata ",
+	e : "xO",
+	T : "U "
+}));
+console.log('hola');
+console.log('soy un archivo de js');
