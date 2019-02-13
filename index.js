282e15c703f460aea43ffdd24acd9ac5dc25532d
Antonio Villavicencio <jantoniovc@me.com>
2019-02-12 20:23:18 -0600

    Add random character and write to file

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const fs = require('fs');
  * Variables
  ***************/
 const edad = Math.ceil(Math.random() * 100);
-const personaje = "vaquita feliz";
+let personaje = "vaquita feliz";
 const lugar = "la granja feliz";
 const amigos = "pollin y cochimon";
 const villano = "Nutria";
@@ -21,19 +21,31 @@ const broma = "perfumo";
 let opcion = "bañarse todas las semanas";
 let end = "sus amigos se reian de ella";
 
-const introHistoria = intro.historia(personaje, lugar, amigos, edad);
-const nudoHistoria = nudo.historia(personaje, villano, broma);
-const finalHistoria = final.historia(opcion, end);
-const historiaCompleta = introHistoria + nudoHistoria + finalHistoria;
-
-fs.writeFile('/Users/JAVC/Desktop/historia.txt', historiaCompleta, (err) => {
+// Read a character
+fs.readFile("personajes.txt", "utf-8", (err, data) => {
     if(err) {
         console.log(err);
+    } else {
+        let personajes = data.split(",");
+        let numberOfCharacters = personajes.length;
+        let randomCharacterIndex = Math.floor(Math.random() * numberOfCharacters);
+        personaje = personajes[randomCharacterIndex];
+        const introHistoria = intro.historia(personaje, lugar, amigos, edad);
+        const nudoHistoria = nudo.historia(personaje, villano, broma);
+        const finalHistoria = final.historia(opcion, end);
+        const historiaCompleta = introHistoria + nudoHistoria + finalHistoria;
+        
+        fs.writeFile('/Users/JAVC/Desktop/historia.txt', historiaCompleta, (err) => {
+            if(err) {
+                console.log(err);
+            }
+        });
+        
+        console.log(cowsay.say({
+            text : `Esta es la historia de ${personaje}, ve el archivo generado`,
+            e : "0 0",
+            T : "U "
+        }));
     }
 });
 
-console.log(cowsay.say({
-	text : "Hola amigos ",
-	e : "0 0",
-	T : "U "
-}));
